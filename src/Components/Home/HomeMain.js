ee3946e5b32ff48de8fc7e1892456debbb2f405d
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-06-02 21:35:51 +0530

    add typing effect in home

--- src/Components/Home/HomeMain.js
+++ src/Components/Home/HomeMain.js
@@ -1,22 +1,35 @@
+import React from "react";
 import "./homeMain.css";
 import GIF from "./../../assets/giphy.webp";
+import Typing from "./../Common/typing.js";
 
-function homeMain() {
+function HomeMain() {
   const homeMain = (
-    <div class="container">
-      <div class="main-home-text">
+    <div className="container">
+      <div className="main-home-text">
         <h1>Hi There!</h1>
         <h2>
-          I'm <span class="name">KESHAV MAKKAD</span>
+          I'm <span className="name">KESHAV MAKKAD</span>
         </h2>
-        <h3>Full Stack Developer</h3>
+        <h3>
+          <Typing
+            text={[
+              "Full Stack Developer",
+              "Tech Enthusiast",
+              "Lifelong Learner",
+            ]}
+            typingSpeed={100}
+            deletingSpeed={50}
+            duration={1000}
+          />
+        </h3>
       </div>
-      <div class="image-home">
-        <img src={GIF} alt="Dev pic" class="dev-pic" />
+      <div className="image-home">
+        <img src={GIF} alt="Dev pic" className="dev-pic" />
       </div>
     </div>
   );
   return homeMain;
 }
 
-export default homeMain;
+export default HomeMain;
