8735d83482408ea32c69c745d4d3ff41ee60bc35
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-06-05 12:43:20 +0530

    add profile icon

--- src/Components/Common/navbar.js
+++ src/Components/Common/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import profileIMG from "./../../assets/360_F_391695153_SP2ROLAgm3OltQIyIH916vYogXGdR9Dj.jpg";
+import profileIMG from "./../../assets/logo.jpg";
 <link
   rel="stylesheet"
   href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
